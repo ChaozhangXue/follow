0bbac5289b91d4456859ac5eaa3e75ac12a6776a
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-06-27 14:34:38 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,14 +3,18 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=follow',
             'username' => 'root',
-            'password' => '',
+            'password' => 'del-cache123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
         ],
     ],
 ];
