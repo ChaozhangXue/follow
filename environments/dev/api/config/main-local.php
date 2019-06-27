3a7fdb898c3b435c536108c66c2eb0517e4d0f18
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-06-27 14:30:14 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'api-O2HowPngjH4Eq9m_hAjfYicmRMSosaDM',
         ],
     ],
 ];
@@ -12,9 +12,9 @@ $config = [
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
+    $config['modules']['debug']['class'] = 'yii\debug\Module';
+    $config['modules']['debug']['allowedIPs'] = ['*', '127.0.0.1', '0.0.0.0'];
+    $config['modules']['debug']['historySize'] = 200;
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
