50674222f18e0bed829c198602d780e02fe8f461
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-06-28 16:27:49 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- backend/controllers/FileController.php
+++ backend/controllers/FileController.php
@@ -19,7 +19,7 @@ class FileController extends Controller
         // 制定允许其他域名访问
         header("Access-Control-Allow-Origin:*");
         // 响应类型
-        header('Access-Control-Allow-Methods:POST');
+        header('Access-Control-Allow-Methods:*');
         // 响应头设置
         header('Access-Control-Allow-Headers:x-requested-with, content-type');
 
