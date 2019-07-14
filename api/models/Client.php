69cdbabb8315e12c5a67e89efab345119590f56d
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-07-15 00:28:49 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- api/models/Client.php
+++ api/models/Client.php
@@ -16,18 +16,18 @@ use Yii;
  * @property int $age 客户年龄
  * @property string $position 客户职位
  * @property string $country 所属国家
- * @property int $nature 公司性质(1.服装厂 2.批发商 3代理商 4零售商)
  * @property string $first_follow_time 首次接触时间
+ * @property int $nature 公司性质(1.服装厂 2.批发商 3代理商 4零售商)
  * @property int $sex 性别 1：男 2 女
  * @property int $intention 客户意向 1：偏低 2：中等 3： 偏高
  * @property int $type 客户类型 1：新客户 2：老客户
  * @property int $distribute 客户分配 1：待分配 2：已分配
  * @property int $source 客户来源 1：展会 2线上 3客户介绍
  * @property string $follow_backup 来源备注
+ * @property string $make_time 建档时间
  * @property string $follower_name 跟进人姓名
  * @property int $follower_id 跟进人id（参照userinfo表的数据）
- * @property string $make_time 建档时间
- * @property string $maker 建党人员
+ * @property string $maker 建档人员
  * @property string $update_time 修改时间
  * @property string $update_people 修改角色
  */
@@ -68,17 +68,17 @@ class Client extends \yii\db\ActiveRecord
             'age' => 'Age',
             'position' => 'Position',
             'country' => 'Country',
-            'nature' => 'Nature',
             'first_follow_time' => 'First Follow Time',
+            'nature' => 'Nature',
             'sex' => 'Sex',
             'intention' => 'Intention',
             'type' => 'Type',
             'distribute' => 'Distribute',
             'source' => 'Source',
             'follow_backup' => 'Follow Backup',
+            'make_time' => 'Make Time',
             'follower_name' => 'Follower Name',
             'follower_id' => 'Follower ID',
-            'make_time' => 'Make Time',
             'maker' => 'Maker',
             'update_time' => 'Update Time',
             'update_people' => 'Update People',
