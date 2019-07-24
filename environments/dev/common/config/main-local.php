a05d7c278b17f4ed6f1754cbe95af5fce29ffcd6
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-07-24 13:19:13 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=follow',
             'username' => 'root',
-            'password' => '',
+            'password' => 'del-cache123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
