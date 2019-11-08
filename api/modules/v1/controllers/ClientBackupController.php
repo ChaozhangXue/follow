eea8042898c8b9c7702896f68ddb0768a3609a1d
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-11-08 21:50:40 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- api/modules/v1/controllers/ClientBackupController.php
+++ api/modules/v1/controllers/ClientBackupController.php
@@ -2,6 +2,7 @@
 
 namespace api\modules\v1\controllers;
 
+use api\models\ClientBackup;
 use api\models\Userinfo;
 use Yii;
 use api\controllers\BaseController;
@@ -9,4 +10,33 @@ use api\controllers\BaseController;
 class ClientBackupController extends BaseController
 {
     public $modelClass = 'api\models\ClientBackup';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        // 禁用""index,delete" 和 "create" 操作
+        unset($actions['update']);
+        return $actions;
+    }
+
+    public function actionUpdate(){
+        $client_id = Yii::$app->request->post('client_id');
+        $post = Yii::$app->request->post();
+
+        $client_price = ClientBackup::find()->where(['client_id' => $client_id])->one();
+        if(empty($client_price)){
+            //新增
+            $price = new ClientBackup();
+        }else{
+            //更新
+            $price = $client_price;
+        }
+
+        foreach ($post as $key =>$val){
+            $price->$key = $val;
+        }
+        $price->save();
+
+        return $this->success();
+    }
 }
--- api/modules/v1/controllers/ClientContactController.php
+++ api/modules/v1/controllers/ClientContactController.php
@@ -2,11 +2,39 @@
 
 namespace api\modules\v1\controllers;
 
+use api\models\ClientContact;
 use Yii;
 use api\controllers\BaseController;
 
 class ClientContactController extends BaseController
 {
     public $modelClass = 'api\models\ClientContact';
+    public function actions()
+    {
+        $actions = parent::actions();
+        // 禁用""index,delete" 和 "create" 操作
+        unset($actions['update']);
+        return $actions;
+    }
 
+    public function actionUpdate(){
+        $client_id = Yii::$app->request->post('client_id');
+        $post = Yii::$app->request->post();
+
+        $client_price = ClientContact::find()->where(['client_id' => $client_id])->one();
+        if(empty($client_price)){
+            //新增
+            $price = new ClientContact();
+        }else{
+            //更新
+            $price = $client_price;
+        }
+
+        foreach ($post as $key =>$val){
+            $price->$key = $val;
+        }
+        $price->save();
+
+        return $this->success();
+    }
 }
