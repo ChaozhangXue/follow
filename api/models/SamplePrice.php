c56b80d1b218dba06d3cd6270be367c633d319b8
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-11-08 22:16:24 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- api/models/SamplePrice.php
+++ api/models/SamplePrice.php
@@ -3,7 +3,6 @@
 namespace api\models;
 
 use Yii;
-use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "sample_price".
@@ -15,16 +14,18 @@ use yii\behaviors\TimestampBehavior;
  * @property double $cost 成本价格
  * @property double $make_price 加工价格
  * @property double $dibu_price 底部价格
- * @property double $on_tax_price 含税价格
- * @property double $carriage 含运费价
- * @property string $range_price 区间价格
+ * @property double $on_tax_price 含税含运价格
+ * @property double $carriage 调样价格
+ * @property string $range_price 打样价格
  * @property string $rmb_backup 人民币的备注信息
  * @property double $USD_price 美元价格
- * @property string $trade 贸易条款
+ * @property string $trade  贸易条款
  * @property string $aim 目的港口
- * @property string $CBM cbm
+ * @property string $CBM 货运信息
  * @property string $maitou 唛头
  * @property string $usd_backup 美元的备注信息
+ * @property string $created_at
+ * @property string $updated_at
  */
 class SamplePrice extends \yii\db\ActiveRecord
 {
@@ -36,20 +37,6 @@ class SamplePrice extends \yii\db\ActiveRecord
         return 'sample_price';
     }
 
-    public function behaviors()
-    {
-        return [
-            [
-                'class' => TimestampBehavior::className(),
-                'createdAtAttribute' => 'created_at',
-                'updatedAtAttribute' => 'updated_at',
-                //'value' => new Expression('NOW()'),
-                'value'=>date('Y-m-d H:i:s'),
-            ],
-        ];
-    }
-
-
     /**
      * {@inheritdoc}
      */
@@ -59,6 +46,7 @@ class SamplePrice extends \yii\db\ActiveRecord
             [['sample_id'], 'integer'],
             [['online_price', 'offline_price', 'cost', 'make_price', 'dibu_price', 'on_tax_price', 'carriage', 'USD_price'], 'number'],
             [['rmb_backup', 'usd_backup'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
             [['range_price', 'trade', 'aim', 'CBM', 'maitou'], 'string', 'max' => 50],
             [['sample_id'], 'unique'],
         ];
@@ -70,23 +58,25 @@ class SamplePrice extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'sample_id' => 'Sample ID',
-            'online_price' => 'Online Price',
-            'offline_price' => 'Offline Price',
-            'cost' => 'Cost',
-            'make_price' => 'Make Price',
-            'dibu_price' => 'Dibu Price',
-            'on_tax_price' => 'On Tax Price',
-            'carriage' => 'Carriage',
-            'range_price' => 'Range Price',
-            'rmb_backup' => 'Rmb Backup',
-            'USD_price' => 'Usd Price',
-            'trade' => 'Trade',
-            'aim' => 'Aim',
-            'CBM' => 'Cbm',
-            'maitou' => 'Maitou',
-            'usd_backup' => 'Usd Backup',
+            'id' => '自增长id',
+            'sample_id' => '样品的id',
+            'online_price' => '线上报价',
+            'offline_price' => '线下报价',
+            'cost' => '成本价格',
+            'make_price' => '加工价格',
+            'dibu_price' => '底部价格',
+            'on_tax_price' => '含税含运价格',
+            'carriage' => '调样价格',
+            'range_price' => '打样价格',
+            'rmb_backup' => '人民币的备注信息',
+            'USD_price' => '美元价格',
+            'trade' => ' 贸易条款',
+            'aim' => '目的港口',
+            'CBM' => '货运信息',
+            'maitou' => '唛头',
+            'usd_backup' => '美元的备注信息',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
         ];
     }
 }
