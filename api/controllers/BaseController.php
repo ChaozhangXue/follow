0237e1647cdff6182645363eaa74bfaed84b07fe
rodgexue(薛朝璋) <rodgexue@tencent.com>
2019-11-18 11:56:07 +0800

    Signed-off-by: rodgexue(薛朝璋) <rodgexue@tencent.com>

--- api/controllers/BaseController.php
+++ api/controllers/BaseController.php
@@ -163,7 +163,7 @@ class BaseController extends ActiveController
         $models = $org_model::find()->offset(($pageNum - 1) * $pageSize)->limit($pageSize)->where('1=1');
 
         foreach ($search as $key => $val){
-            if(empty($val) && $val !=0){
+            if(!empty($val) || $val ==0){
                 if($val == 0){
                     $models = $models->andWhere([$key=>trim($val)]);
                 }else{
